a03dd96e19d519abffd280ee0b8990abcd4290d8
pljarlegan <pl.jarlegan@citae.fr>
2019-10-22 14:22:29 +0200

    ADD: ComicList Component

---
+++ src/components/ComicList/index.js
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react';
+import MaterialTable from 'material-table';
+import Button from '@material-ui/core/Button';
+import Context from "../../provider/apollo-graphql";
+
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles({
+  avatar: {
+    margin: 10,
+  },
+  bigAvatar: {
+    margin: 10,
+    width: 60,
+    height: 60,
+  },
+});
+
+export default function ComicsList() {
+  const client = useContext(Context);
+  const classes = useStyles();
+  const comics = {
+    columns: [
+      { title: 'Comic', field: 'id', render: rowData => <Button size="small" color="primary" component={Link} to={`/heros/${rowData.id}`}>{rowData.title}</Button>},
+      { title: 'Characters', field: 'characters.available'},
+      {
+        title: '',
+        field: 'image',
+        render: rowData => <Avatar alt={rowData.name} src={rowData.thumbnail ? `${rowData.thumbnail.path}.${rowData.thumbnail.extension}` : "/empty-avatar.png"} className={classes.bigAvatar}/>
+      }
+    ],
+    data: [],
+  };
+
+  return (
+    <div>
+      <MaterialTable
+        title="Comics"
+        columns={comics.columns}
+        data = { query => {
+          // console.log(JSON.stringify(query, null, 2));
+          return client.paginateQuery("comics", `
+        {
+          comics(limit: ${query.pageSize}, offset: ${query.page*query.pageSize}) {
+            meta {
+              offset
+              limit
+              total
+              count
+            }
+            data {
+              id
+              title
+              description
+              characters {
+                available
+                returned
+                items {
+                  name
+                  role
+                }
+              }
+              thumbnail {
+                path
+                extension
+              }
+            }
+          }
+        }
+        `)
+        }}
+        options={{
+          search: false,
+          actionsColumnIndex: -1
+        }}
+      />
+    </div>
+  );
+}
