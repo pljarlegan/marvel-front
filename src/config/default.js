b83b82e0eb0c1fef3ad9d3f2ddfaea72b4165d66
pljarlegan <pl.jarlegan@citae.fr>
2019-10-23 16:57:15 +0200

    FIX: add default value (test)

--- src/config/default.js
+++ src/config/default.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 module.exports = {
   graphql: {
-    endpoint: window._env_.GRAPHQL_ENDPOINT,
+    endpoint: window._env_ ? window._env_.GRAPHQL_ENDPOINT : "http://127.0.0.1:4000",
   },
 };
